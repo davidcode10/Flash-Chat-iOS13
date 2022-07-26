110fbfb0931d434a1b816b4ceb7650d00065fbbb
davidcode10 <davidedson2030@outlook.com>
2022-07-26 18:00:34 +0200

    ChatApp completed

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,12 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by David Santiago on 16/6/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+struct Message {
+    let sender: String
+    let body: String
+}
